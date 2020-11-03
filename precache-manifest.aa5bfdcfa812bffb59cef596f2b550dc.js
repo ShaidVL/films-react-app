6cfd171a09cfa8c447067654328f7dbe9ddd8479
ilsur <ilsur131@yandex.ru>
2020-11-03 20:37:02 +0300

    Updates

---
+++ precache-manifest.aa5bfdcfa812bffb59cef596f2b550dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f256c636f0ba39a3112a462422183b44",
+    "url": "/test-react-app/index.html"
+  },
+  {
+    "revision": "2b17d0694c38509abe5c",
+    "url": "/test-react-app/static/css/main.c457c405.chunk.css"
+  },
+  {
+    "revision": "6c0ef85e203dcb658202",
+    "url": "/test-react-app/static/js/2.8b469dca.chunk.js"
+  },
+  {
+    "revision": "2b17d0694c38509abe5c",
+    "url": "/test-react-app/static/js/main.058196a1.chunk.js"
+  },
+  {
+    "revision": "6c0cd1c41ecd483c7b51",
+    "url": "/test-react-app/static/js/runtime-main.44ae9e1b.js"
+  },
+  {
+    "revision": "6a070275c307697e5e2024a2704bec58",
+    "url": "/test-react-app/static/media/bookmarkfilled.6a070275.png"
+  }
+]);
